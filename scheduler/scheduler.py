e976576a57bba53a367c959d559b5ee71584be2a
PongpisutFlook <66070124@kmitl.ac.th>
2025-09-18 14:39:05 +0000

    Add Sceduler app

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -2,13 +2,14 @@ import time
 from bson import json_util
 from producer import produce
 from database import get_router_info
-
+import os
 
 def scheduler():
 
     INTERVAL = 30.0
     next_run = time.monotonic()
     count = 0
+    host = os.getenv("RABBITMQ_HOST")
 
     while True:
         now = time.time()
@@ -20,7 +21,7 @@ def scheduler():
         try:
             for data in get_router_info():
                 body_bytes = json_util.dumps(data).encode("utf-8")
-                produce("rabbitmq", body_bytes)
+                produce(host, body_bytes)
         except Exception as e:
             print(e)
             time.sleep(3)
