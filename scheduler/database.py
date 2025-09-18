82b3cd56fa0323f270ec01f5f65dfa9d42af68b7
PongpisutFlook <66070124@kmitl.ac.th>
2025-09-18 18:33:19 +0000

    f

--- scheduler/database.py
+++ scheduler/database.py
@@ -6,10 +6,10 @@ from pymongo import MongoClient
 def get_router_info():
     mongo_uri = os.environ.get("MONGO_URI")
     db_name = os.environ.get("DB_NAME")
-
     client = MongoClient(mongo_uri)
     db = client[db_name]
     routers = db["routers"]
+    print(mongo_uri, router_data)
 
     router_data = routers.find()
     return router_data
